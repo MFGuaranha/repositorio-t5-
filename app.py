b6b5087770ea10d6593fb77a62e127620687f516
Manoel Franciso Guaranha <manoel.guaranha@gmail.com>
2026-07-26 18:44:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ if st.button("🚀 Processar Frase", type="primary"):
         json_mock_retorno = {
             "frame": "Sending",
             "arguments": {
-                "Theme":,       # CORRIGIDO: Mapeia os índices de "o relatório"
+                "Theme":,  None,     # CORRIGIDO: Mapeia os índices de "o relatório"
                 "Recipient": [4, 5]    # Mapeia os índices de "o Diretor"
             }
         }
